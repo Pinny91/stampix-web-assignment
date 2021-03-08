75121d28204a212174adbc7b5dfc1acfcb1324e1
Pinny <pinnewaert.matthias@gmail.com>
2021-03-08 18:26:37 +0100

    Added tests to check language

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
+import React from "react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import App from "./App";
+test("renders correct text", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const text = screen.getByText(/START PRINTING/i);
+  expect(text).toBeInTheDocument();
+});
+test("Language switching", async () => {
+  render(<App />);
+  fireEvent.click(screen.getByText("NL"));
+  await waitFor(() => {
+    // getByRole throws an error if it cannot find an element
+    const text = screen.getByText(/START PRINTEN/i);
+    expect(text).toBeInTheDocument();
+  });
 });
